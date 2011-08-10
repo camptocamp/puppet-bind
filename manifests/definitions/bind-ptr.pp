b133f19a0e73adb11768bd79c897da9ab5473d51
Raffael Schmid <raffael@yux.ch>
2011-08-10 11:22:33 +0200

    add IN PTR support

---
+++ manifests/definitions/bind-ptr.pp
@@ -0,0 +1,27 @@
+/*
+
+= Definition: bind::ptr
+Creates an PTR record.
+
+Arguments:
+ *$zone*:  Bind::Zone name
+ *$owner*: owner of the Resource Record
+ *$host*:  target of the Resource Record
+ *$ttl*:   Time to Live for the Resource Record. Optional.
+
+*/
+define bind::ptr($ensure=present,
+    $zone,
+    $owner=false,
+    $host,
+    $ttl=false) {
+
+  bind::record {$name:
+    ensure => $ensure,
+    zone   => $zone,
+    owner  => $owner,
+    host   => $host,
+    ttl    => $ttl,
+    record_type => 'PTR',
+  }
+}
