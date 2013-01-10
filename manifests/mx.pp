fba4bdbbab794d46f4fe00f0aa4e60458d33f3f7
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2013-01-10 12:01:48 +0100

    bind::mx: remove problematic require
    Dependencies within this module must largely be assured by the use
    of concat. This require seems useless and can actually create
    dependency loops.

--- manifests/mx.pp
+++ manifests/mx.pp
@@ -28,7 +28,6 @@ define bind::mx (
     target  => "/etc/bind/pri/${zone}.conf",
     content => template('bind/mx-record.erb'),
     notify  => Service['bind9'],
-    require => Bind::Zone[$zone],
   }
 
 }
