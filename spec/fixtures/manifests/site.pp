c0d885b51ccdf0aed34aa5dac6dcafebbec61648
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-10 11:37:10 +0200

    Add zone to default node in fixtures

--- spec/fixtures/manifests/site.pp
+++ spec/fixtures/manifests/site.pp
@@ -3,4 +3,11 @@ $concat_basedir = '/var/lib/puppet/concat'
 
 # Default node to test defines
 node default {
+  bind::zone {'test.tld':
+    zone_contact => 'contact.test.tld',
+    zone_ns      => 'ns0.test.tld',
+    zone_serial  => '2012112901',
+    zone_ttl     => '604800',
+    zone_origin  => 'test.tld',
+  }
 }
