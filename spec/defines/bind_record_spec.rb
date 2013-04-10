a682952aecf7795c9cbeb56acf81c0de0c5aa2db
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-10 11:59:41 +0200

    Add tests for bind::record

--- spec/defines/bind_record_spec.rb
+++ spec/defines/bind_record_spec.rb
@@ -20,4 +20,50 @@ describe 'bind::record' do
       }.to raise_error(Puppet::Error, /\$ensure must be either.* got 'running'/)
     end
   end
+
+  context 'when zone is not specified' do
+    let (:params) { {
+      :hash_data   => {},
+      :record_type => 'CNAME'
+    } }
+
+    it 'should fail' do
+      expect { should contain_concat__fragment('') 
+      }.to raise_error(Puppet::Error, /Must pass zone to Bind::Record/)
+    end
+  end
+
+  context 'when hash_data is not specified' do
+    let (:params) { {
+      :zone        => 'foo.example.com',
+      :record_type => 'CNAME'
+    } }
+
+    it 'should fail' do
+      expect { should contain_concat__fragment('') 
+      }.to raise_error(Puppet::Error, /Must pass hash_data to Bind::Record/)
+    end
+  end
+
+  context 'when record_type is not specified' do
+    let (:params) { {
+      :zone        => 'foo.example.com',
+      :hash_data   => {}
+    } }
+
+    it 'should fail' do
+      expect { should contain_concat__fragment('') 
+      }.to raise_error(Puppet::Error, /Must pass record_type to Bind::Record/)
+    end
+  end
+
+  context 'when using default content_template' do
+    let (:params) { {
+      :zone        => 'foo.example.com',
+      :hash_data   => {},
+      :record_type => 'CNAME'
+    } }
+
+    it { should contain_concat__fragment('foo.example.com.CNAME.CNAME foo.example.com') }
+  end
 end
