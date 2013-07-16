8b6b88bf74bdc3eac2ca3b4ad737d8f9cee2856c
Maxence Dunnewind <maxence.dunnewind@inria.fr>
2013-07-16 10:33:37 +0200

    Parametrize tests. There is now a spec/defines/parameters.rb which defines the different OS supported, and the list of corresponding variables. _spec files then iterates over the different OS and run tests that are parametrized with the variables if needed.

--- spec/defines/bind_a_spec.rb
+++ spec/defines/bind_a_spec.rb
@@ -1,150 +1,153 @@
 require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__)) + '/parameters.rb'
 
-describe 'bind::a' do
-  let (:title) { 'foo.example.com' }
-  let (:facts) { {
-    :osfamily        => 'Debian',
-    :operatingsystem => 'Debian'
-  } }
-
-  context 'when using a wrong ensure value' do
-    let (:params) { {
-      :ensure      => 'running',
-      :zone        => 'foo.example.com',
-      :hash_data   => {},
+@parameters.each { |k, v|
+  describe 'bind::a' do
+    let (:title) { 'foo.example.com' }
+    let (:facts) { {
+      :osfamily        => v['osfamily'],
+      :operatingsystem => k
     } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /\$ensure must be either.* got 'running'/)
+    context 'when using a wrong ensure value' do
+      let (:params) { {
+        :ensure      => 'running',
+        :zone        => 'foo.example.com',
+        :hash_data   => {},
+      } }
+
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /\$ensure must be either.* got 'running'/)
+        end
+        end
+
+    context 'when zone is not specified' do
+      let (:params) { {
+        :hash_data   => {},
+      } }
+
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /Must pass zone to Bind::A/)
+      end
     end
-  end
 
-  context 'when zone is not specified' do
-    let (:params) { {
-      :hash_data   => {},
-    } }
+    context 'when passing wrong type for zone' do
+      let (:params) { {
+        :zone      => ['foo.example.com'],
+        :hash_data => {},
+      } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /Must pass zone to Bind::A/)
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /\["foo.example.com"\] is not a string\./)
+      end
     end
-  end
 
-  context 'when passing wrong type for zone' do
-    let (:params) { {
-      :zone      => ['foo.example.com'],
-      :hash_data => {},
-    } }
+    context 'when hash_data is not specified' do
+      let (:params) { {
+        :zone        => 'foo.example.com',
+      } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /\["foo.example.com"\] is not a string\./)
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /Must pass hash_data to Bind::A/)
+      end
     end
-  end
 
-  context 'when hash_data is not specified' do
-    let (:params) { {
-      :zone        => 'foo.example.com',
-    } }
+    context 'when passing wrong type for hash_data' do
+      let (:params) { {
+        :zone      => 'foo.example.com',
+        :hash_data => 'bar',
+      } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /Must pass hash_data to Bind::A/)
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /"bar" is not a Hash\./)
+      end
     end
-  end
 
-  context 'when passing wrong type for hash_data' do
-    let (:params) { {
-      :zone      => 'foo.example.com',
-      :hash_data => 'bar',
-    } }
+    context 'when passing wrong type for zone_arpa' do
+      let (:params) { {
+        :zone      => 'foo.example.com',
+        :hash_data => {},
+        :zone_arpa => ['bar']
+      } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /"bar" is not a Hash\./)
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /\["bar"\] is not a string\./)
+      end
     end
-  end
 
-  context 'when passing wrong type for zone_arpa' do
-    let (:params) { {
-      :zone      => 'foo.example.com',
-      :hash_data => {},
-      :zone_arpa => ['bar']
-    } }
+    context 'when passing wrong type for ptr' do
+      let (:params) { {
+        :zone      => 'foo.example.com',
+        :hash_data => {},
+        :ptr       => 'false',
+      } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /\["bar"\] is not a string\./)
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /"false" is not a boolean\./)
+      end
     end
-  end
 
-  context 'when passing wrong type for ptr' do
-    let (:params) { {
-      :zone      => 'foo.example.com',
-      :hash_data => {},
-      :ptr       => 'false',
-    } }
+    context 'when passing ptr without zone_arpa' do
+      let (:params) { {
+        :zone      => 'foo.example.com',
+        :hash_data => {},
+        :ptr       => true,
+        :zone_arpa => ''
+      } }
 
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /"false" is not a boolean\./)
+      it 'should fail' do
+        expect { should contain_bind__record('foo.example.com')
+        }.to raise_error(Puppet::Error, /You need zone_arpa if you want the PTR/)
+      end
     end
+
+    context 'when using not using ptr' do
+      let (:params) { {
+        :zone      => 'foo.example.com',
+        :hash_data => {},
+        :ptr       => false
+      } }
+
+      it { should contain_bind__record('foo.example.com').with(
+        :ensure           => 'present',
+        :zone             => 'foo.example.com',
+        :record_type      => 'A',
+        :hash_data        => {},
+        :content_template => nil
+      ) }
+        end
+
+    context 'when using using ptr' do
+      let (:params) { {
+        :zone      => 'foo.example.com',
+        :hash_data => {},
+        :ptr       => true,
+        :zone_arpa => 'foobar.arpa'
+      } }
+
+      it { should contain_bind__record('foo.example.com').with(
+        :ensure           => 'present',
+        :zone             => 'foo.example.com',
+        :record_type      => 'A',
+        :hash_data        => {},
+        :content_template => nil
+      ) }
+
+      it { should contain_bind__record('PTR foo.example.com').with(
+        :ensure           => 'present',
+        :zone             => 'foobar.arpa',
+        :record_type      => 'PTR',
+        :ptr_zone         => 'foo.example.com',
+        :hash_data        => {},
+        :content_template => nil
+      ) }
+        end
   end
-
-  context 'when passing ptr without zone_arpa' do
-    let (:params) { {
-      :zone      => 'foo.example.com',
-      :hash_data => {},
-      :ptr       => true,
-      :zone_arpa => ''
-    } }
-
-    it 'should fail' do
-      expect { should contain_bind__record('foo.example.com')
-      }.to raise_error(Puppet::Error, /You need zone_arpa if you want the PTR/)
-    end
-  end
-
-  context 'when using not using ptr' do
-    let (:params) { {
-      :zone      => 'foo.example.com',
-      :hash_data => {},
-      :ptr       => false
-    } }
-
-    it { should contain_bind__record('foo.example.com').with(
-      :ensure           => 'present',
-      :zone             => 'foo.example.com',
-      :record_type      => 'A',
-      :hash_data        => {},
-      :content_template => nil
-    ) }
-  end
-
-  context 'when using using ptr' do
-    let (:params) { {
-      :zone      => 'foo.example.com',
-      :hash_data => {},
-      :ptr       => true,
-      :zone_arpa => 'foobar.arpa'
-    } }
-
-    it { should contain_bind__record('foo.example.com').with(
-      :ensure           => 'present',
-      :zone             => 'foo.example.com',
-      :record_type      => 'A',
-      :hash_data        => {},
-      :content_template => nil
-    ) }
-
-    it { should contain_bind__record('PTR foo.example.com').with(
-      :ensure           => 'present',
-      :zone             => 'foobar.arpa',
-      :record_type      => 'PTR',
-      :ptr_zone         => 'foo.example.com',
-      :hash_data        => {},
-      :content_template => nil
-    ) }
-  end
-end
+}
--- spec/defines/bind_key_spec.rb
+++ spec/defines/bind_key_spec.rb
@@ -1,53 +1,56 @@
 require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__)) + '/parameters.rb'
 
-describe 'bind::key' do
-  let (:title) {'update.domain.tld'}
-  let (:facts) { {
-    :osfamily        => 'Debian',
-    :operatingsystem => 'Debian'
-  } }
-
-  # Validate input
-  context 'when missing secret value' do
-    it 'should fail' do
-      expect { should contain_concat('/etc/bind/keys/update.domain.tld.conf')
-      }.to raise_error(Puppet::Error, /Must pass secret to Bind::Key.*/)
-    end
-  end
-
-  context 'when using a wrong ensure value' do
-    let (:params) { {
-      :ensure => 'running',
-      :secret => 'abcdefg'
-    } }
-    
-    it 'should fail' do
-      expect { should contain_concat('/etc/bind/keys/update.domain.tld.conf')
-      }.to raise_error(Puppet::Error, /\$ensure must be either.* got 'running'/)
-    end
-  end
-
-  context 'when passing wrong type for algorithm' do
-    let (:params) { {
-      :secret    => 'abcdefg',
-      :algorithm => ['abcde', 'fghij']
+@parameters.each { |k, v|
+  describe 'bind::key' do
+    let (:title) {'update.domain.tld'}
+    let (:facts) { {
+      :osfamily        => v['osfamily'],
+      :operatingsystem => k
     } }
 
-    it 'should fail' do
-      expect { should contain_concat('/etc/bind/keys/update.domain.tld.conf')
-      }.to raise_error(Puppet::Error, /\["abcde", "fghij"\] is not a string\..+/)
+    # Validate input
+    context 'when missing secret value' do
+      it 'should fail' do
+        expect { should contain_concat("#{v['keys_directory']}/update.domain.tld.conf")
+        }.to raise_error(Puppet::Error, /Must pass secret to Bind::Key.*/)
+      end
     end
-  end
 
-  context 'when passing wrong type for secret' do
-    let (:params) { {
-      :secret => ['abcde', 'fghij']
-    } }
+    context 'when using a wrong ensure value' do
+      let (:params) { {
+        :ensure => 'running',
+        :secret => 'abcdefg'
+      } }
 
-    it 'should fail' do
-      expect { should contain_concat('/etc/bind/keys/update.domain.tld.conf')
-      }.to raise_error(Puppet::Error, /\["abcde", "fghij"\] is not a string\..+/)
+      it 'should fail' do
+        expect { should contain_concat("#{v['keys_directory']}/update.domain.tld.conf")
+        }.to raise_error(Puppet::Error, /\$ensure must be either.* got 'running'/)
+        end
+        end
+
+    context 'when passing wrong type for algorithm' do
+      let (:params) { {
+        :secret    => 'abcdefg',
+        :algorithm => ['abcde', 'fghij']
+      } }
+
+      it 'should fail' do
+        expect { should contain_concat("#{v['keys_directory']}/update.domain.tld.conf")
+        }.to raise_error(Puppet::Error, /\["abcde", "fghij"\] is not a string\..+/)
+      end
     end
-  end
 
-end
+    context 'when passing wrong type for secret' do
+      let (:params) { {
+        :secret => ['abcde', 'fghij']
+      } }
+
+      it 'should fail' do
+        expect { should contain_concat("#{v['keys_directory']}/update.domain.tld.conf")
+        }.to raise_error(Puppet::Error, /\["abcde", "fghij"\] is not a string\..+/)
+      end
+    end
+
+  end
+}
