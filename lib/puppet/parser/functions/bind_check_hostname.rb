867ed51cc6237361a6b82cde975980e7b23f0cb7
braddeicide <braddeicide@hotmail.com>
2014-01-03 18:21:41 +1100

    added ? to regex to allow * and *.domain.com.
    This allows * and *.foo.com.
    Needed to catch '' because is_domain_name fails empty.

--- lib/puppet/parser/functions/bind_check_hostname.rb
+++ lib/puppet/parser/functions/bind_check_hostname.rb
@@ -3,7 +3,7 @@
 #
 module Puppet::Parser::Functions
   newfunction(:bind_check_hostname, :type => :rvalue, :doc => <<-EOS
-Prepare checked string for *is_domain_name()* (from stdlib) by removing /^\*\./
+Prepare checked string for *is_domain_name()* (from stdlib) by removing /^\*\.?/
 if present. *is_domain_name()* doesn't want any wildcard, which makes sense in
 most cases.
 Usage: bind_check_hostname(hostname, type)
@@ -21,9 +21,6 @@ Usage: bind_check_hostname(hostname, type)
     # Allows '@'
     return true if record == '@'
     
-    # Allows '*'
-    return true if record == '*'
-
     # All is allowed for SRV and TXT record types
     return true if type == 'SRV'
     return true if type == 'TXT'
@@ -33,7 +30,11 @@ Usage: bind_check_hostname(hostname, type)
     # which doesn't accept wildcards, we just clean it
     # from the record, and pass this new string to
     # *is_domain_name()*
-    domain = record.sub(/^\*\./, '')
+    domain = record.sub(/^\*\.?/, '')
+
+    # Nothing left to check, and is_domain_name fails empty
+    return true if domain == ''
+
     return function_is_domain_name([domain])
   end
 end
