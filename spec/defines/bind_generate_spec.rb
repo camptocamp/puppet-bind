ce5709d6631e2c4d9cf77536616441362ac4e585
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2016-02-08 10:05:47 +0100

    Changed/updated resource name
    This allows to get multiple "similar" generate ranges. For exemple:
    
      bind::generate {'PTR lab lsn':
        zone         => '27.10.in-addr.arpa',
        range        => '100-199',
        record_type  => 'PTR',
        record_class => 'IN',
        lhs          => '$.30.27.10.in-addr.arpa.',
        rhs          => 'lab$.lab.lsn.camptocamp.com.',
      }
    
      bind::generate {'PTR guest lsn':
        zone        => '27.10.in-addr.arpa',
        range       => '100-199',
        record_type => 'PTR',
        lhs         => '$.12.27.10.in-addr.arpa.',
        rhs         => 'guest$.gst.lsn.camptocamp.com.',
      }
    
    And so on.
    
    The previous name construction was creating two resources with this name:
    27.10.in-addr.arpa.PTR.100-199.generate
    
    Although the resource content would be different.

--- spec/defines/bind_generate_spec.rb
+++ spec/defines/bind_generate_spec.rb
@@ -227,7 +227,7 @@ describe 'bind::generate' do
           :lhs         => 'dhcp-$',
           :rhs         => '10.10.0.$',
         } }
-        it { should contain_concat__fragment('test.tld.A.2-100.generate').with({
+        it { should contain_concat__fragment('a-record.generate').with({
           :ensure  => 'present',
           :target  => "#{confdir}/pri/test.tld.conf",
           :content => "\$GENERATE 2-100 dhcp-\$   A 10.10.0.\$ ; a-record\n"
@@ -243,7 +243,7 @@ describe 'bind::generate' do
           :lhs         => 'dhcp-$',
           :rhs         => '10.10.0.$',
         } }
-        it { should contain_concat__fragment('test.tld.CNAME.2-100.generate').with({
+        it { should contain_concat__fragment('a-record.generate').with({
           :ensure  => 'present',
           :target  => "#{confdir}/pri/test.tld.conf",
           :content => "\$GENERATE 2-100 dhcp-\$   CNAME 10.10.0.\$ ; a-record\n"
@@ -259,7 +259,7 @@ describe 'bind::generate' do
           :lhs         => '$.0.10.10.IN-ADDR.ARPA.',
           :rhs         => 'dhcp-$.test.tld.',
         } }
-        it { should contain_concat__fragment('0.10.10.IN-ADDR.ARPA.PTR.2-100.generate').with({
+        it { should contain_concat__fragment('ptr-record.generate').with({
           :ensure  => 'present',
           :target  => "#{confdir}/pri/0.10.10.IN-ADDR.ARPA.conf",
           :content => "$GENERATE 2-100 $.0.10.10.IN-ADDR.ARPA.   PTR dhcp-$.test.tld. ; ptr-record\n"
