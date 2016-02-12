12f915a38b5ca834092b72fed2164c209df13128
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2016-02-12 08:06:37 +0100

    separate tests so that we know WHAT crashes

--- spec/acceptance/bind_spec.rb
+++ spec/acceptance/bind_spec.rb
@@ -3,12 +3,24 @@ require 'spec_helper_acceptance'
 describe 'bind' do
 
   context 'with defaults' do
-    it 'should idempotently run' do
+    it 'should apply without error' do
       pp = <<-EOS
         class { 'bind': }
       EOS
 
       apply_manifest(pp, :catch_failures => true)
+    end
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'bind': }
+      EOS
+
+      apply_manifest(pp, :catch_changes => true)
+    end
+    it 'should idempotently run bis' do
+      pp = <<-EOS
+        class { 'bind': }
+      EOS
       apply_manifest(pp, :catch_changes => true)
     end
   end
