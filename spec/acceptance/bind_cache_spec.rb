da5e528d8b05ff050a89486e0c29df209963c4f1
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2016-02-15 15:22:58 +0100

    new acceptance check in order to ensure configuration options are properly taken

---
+++ spec/acceptance/bind_cache_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper_acceptance'
+
+describe 'bind' do
+
+  let(:serial) { '2016021209' }
+
+  context 'with defaults' do
+    it 'should apply without error' do
+      pp = <<-EOS
+        class { 'bind':
+          config => {
+            'allow-query'       => [
+              'any',
+            ],
+            'allow-query-cache' => [
+              '127.0.0.0/8',
+              '::1',
+            ],
+            'allow-recursion'   => [
+              'any',
+            ],
+            'forward'           => 'first',
+            'forwarders'        => [
+              '8.8.8.8',
+              '8.8.4.4',
+            ],
+            'listen-on'         => [
+              'any',
+            ],
+            'listen-on-v6'      => [
+              'any',
+            ],
+            'max-cache-ttl'     => '300',
+            'max-ncache-ttl'    => '300',
+          },
+          default_view          => {
+            'recursion'         => 'yes',
+          },
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe command("host -4 google-public-dns-a.google.com localhost") do
+      its(:stdout) {should match /google-public-dns-a.google.com has address 8.8.8.8/}
+    end
+    describe command("host -4 www.camptocamp.com localhost") do
+      its(:stdout) {should match /has address/}
+    end
+  end
+
+end
